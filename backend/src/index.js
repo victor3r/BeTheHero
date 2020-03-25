b8fa9bbd74e186186823735b5737b8b51b1585de
Victor Rabelo <victorabelo1697@gmail.com>
2020-03-25 13:19:13 -0300

    Criando a base da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ */
+
+/**
+ * Driver: SELECT * FROM users
+ * Query Builder: table('users').select('*').where()
+ */
+
+app.listen(3333);
